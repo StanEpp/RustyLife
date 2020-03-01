cd342da8ef7645dc684d65d67962c4cbd6eb4a92
StanEpp <>
2020-03-01 04:46:24 +0100

    Parallelize lifecycle algorithm. Fix set_cell() method. Align grid width to multiple of 16.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ mod game_of_life;
 
 
 fn main() {
-    let mut gof = game_of_life::GameOfLife::new((48, 48),
+    let mut gof = game_of_life::GameOfLife::new((1000, 1000),
                                                 String::from("Game of Life"),
                                                 (1920, 1080));
     gof.run();
 }
-
