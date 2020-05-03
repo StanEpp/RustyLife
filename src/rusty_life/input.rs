d0af1e2da919694c5301024f91f7ef95dbbcf356
StanEpp <>
2020-05-03 14:28:10 +0200

    Remove repetetive code in the innput mod by using macros.

--- src/rusty_life/input.rs
+++ src/rusty_life/input.rs
@@ -6,7 +6,46 @@ use sdl2::mouse::*;
 
 const KEY_MAP_SIZE : usize = 32;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+macro_rules! display_enum {
+    (pub enum $name:ident {
+        $($variant:ident),*,
+    }) => {
+        pub enum $name {
+            $($variant),*
+        }
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                match self {
+                    $($name::$variant => write!(f, stringify!($name::$variant))),*
+                }
+            }
+        }
+    };
+}
+
+macro_rules! match_key_down {
+    ($s:ident, $keycode:ident, $(($sdlKey:ident, $key:ident)),*) => {
+        match $keycode {
+            $(Keycode::$sdlKey => {
+                $s.input_map.keys_pressed[Key::$key] = !$s.input_map.keys_hold[Key::$key];
+                $s.input_map.keys_hold[Key::$key] = true;
+            }),*
+            _ => (),
+        }
+    }
+}
+
+macro_rules! match_key_up {
+    ($s:ident, $keycode:ident, $(($sdlKey:ident, $key:ident)),*) => {
+        match $keycode {
+            $(Keycode::$sdlKey => $s.input_map.keys_hold[Key::$key] = false,)*
+            _ => (),
+        }
+    }
+}
+
+display_enum!{
 pub enum Key {
     W,
     A,
@@ -27,6 +66,7 @@ pub enum Key {
     MouseWheelUp,
     MouseWheelDown,
     NONE,
+}
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -67,84 +107,40 @@ impl Input {
         for event in self.event_pump.poll_iter() {
             match event {
                 sdlEvent::KeyDown {keycode : Some(k), ..} => {
-                    match k {
-                        Keycode::W => {
-                            self.input_map.keys_pressed[Key::W] = !self.input_map.keys_hold[Key::W];
-                            self.input_map.keys_hold[Key::W] = true;
-                        },
-                        Keycode::A => {
-                            self.input_map.keys_pressed[Key::A] = !self.input_map.keys_hold[Key::A];
-                            self.input_map.keys_hold[Key::A] = true;
-                        },
-                        Keycode::S => {
-                            self.input_map.keys_pressed[Key::S] = !self.input_map.keys_hold[Key::S];
-                            self.input_map.keys_hold[Key::S] = true;
-                        },
-                        Keycode::D => {
-                            self.input_map.keys_pressed[Key::D] = !self.input_map.keys_hold[Key::D];
-                            self.input_map.keys_hold[Key::D] = true;
-                        },
-                        Keycode::R => {
-                            self.input_map.keys_pressed[Key::R] = !self.input_map.keys_hold[Key::R];
-                            self.input_map.keys_hold[Key::R] = true;
-                        },
-                        Keycode::N => {
-                            self.input_map.keys_pressed[Key::N] = !self.input_map.keys_hold[Key::N];
-                            self.input_map.keys_hold[Key::N] = true;
-                        },
-                        Keycode::F => {
-                            self.input_map.keys_pressed[Key::F] = !self.input_map.keys_hold[Key::F];
-                            self.input_map.keys_hold[Key::F] = true;
-                        },
-                        Keycode::I => {
-                            self.input_map.keys_pressed[Key::I] = !self.input_map.keys_hold[Key::I];
-                            self.input_map.keys_hold[Key::I] = true;
-                        },
-                        Keycode::LShift => {
-                            self.input_map.keys_pressed[Key::LSHIFT] = !self.input_map.keys_hold[Key::LSHIFT];
-                            self.input_map.keys_hold[Key::LSHIFT] = true;
-                        },
-                        Keycode::RShift => {
-                            self.input_map.keys_pressed[Key::RSHIFT] = !self.input_map.keys_hold[Key::RSHIFT];
-                            self.input_map.keys_hold[Key::RSHIFT] = true;
-                        },
-                        Keycode::KpPlus => {
-                            self.input_map.keys_pressed[Key::NumPLUS] = !self.input_map.keys_hold[Key::NumPLUS];
-                            self.input_map.keys_hold[Key::NumPLUS] = true;
-                        },
-                        Keycode::KpMinus => {
-                            self.input_map.keys_pressed[Key::NumMINUS] = !self.input_map.keys_hold[Key::NumMINUS];
-                            self.input_map.keys_hold[Key::NumMINUS] = true;
-                        },
-                        Keycode::Space => {
-                            self.input_map.keys_pressed[Key::SPACE] = !self.input_map.keys_hold[Key::SPACE];
-                            self.input_map.keys_hold[Key::SPACE] = true;
-                        },
-                        Keycode::Escape => {
-                            self.input_map.keys_pressed[Key::ESC] = !self.input_map.keys_hold[Key::ESC];
-                            self.input_map.keys_hold[Key::ESC] = true;
-                        },
-                        _ => (),
-                    }
+                    match_key_down!(self, k,
+                        (W, W),
+                        (A, A),
+                        (S,S),
+                        (D,D),
+                        (R,R),
+                        (N,N),
+                        (F,F),
+                        (I,I),
+                        (LShift, LSHIFT),
+                        (RShift, RSHIFT),
+                        (KpPlus, NumPLUS),
+                        (KpMinus, NumMINUS),
+                        (Space, SPACE),
+                        (Escape, ESC)
+                    );
                 },
                 sdlEvent::KeyUp {keycode : Some(k), ..} => {
-                    match k {
-                        Keycode::W => self.input_map.keys_hold[Key::W] = false,
-                        Keycode::A => self.input_map.keys_hold[Key::A] = false,
-                        Keycode::S => self.input_map.keys_hold[Key::S] = false,
-                        Keycode::D => self.input_map.keys_hold[Key::D] = false,
-                        Keycode::R => self.input_map.keys_hold[Key::R] = false,
-                        Keycode::N => self.input_map.keys_hold[Key::N] = false,
-                        Keycode::F => self.input_map.keys_hold[Key::F] = false,
-                        Keycode::I => self.input_map.keys_hold[Key::I] = false,
-                        Keycode::KpPlus => self.input_map.keys_hold[Key::NumPLUS] = false,
-                        Keycode::KpMinus => self.input_map.keys_hold[Key::NumMINUS] = false,
-                        Keycode::LShift => self.input_map.keys_hold[Key::LSHIFT] = false,
-                        Keycode::RShift => self.input_map.keys_hold[Key::RSHIFT] = false,
-                        Keycode::Space => self.input_map.keys_hold[Key::SPACE] = false,
-                        Keycode::Escape => self.input_map.keys_hold[Key::ESC] = false,
-                        _ => ()
-                    }
+                    match_key_up!(self, k,
+                        (W, W),
+                        (A, A),
+                        (S,S),
+                        (D,D),
+                        (R,R),
+                        (N,N),
+                        (F,F),
+                        (I,I),
+                        (LShift, LSHIFT),
+                        (RShift, RSHIFT),
+                        (KpPlus, NumPLUS),
+                        (KpMinus, NumMINUS),
+                        (Space, SPACE),
+                        (Escape, ESC)
+                    );
                 },
                 sdlEvent::MouseButtonDown {mouse_btn, ..} => {
                     match mouse_btn {
@@ -203,31 +199,5 @@ impl std::ops::Index<Key> for [bool; KEY_MAP_SIZE] {
     fn index(&self, idx : Key) -> &bool {
         let this : &[bool] = self;
         &this[idx as usize]
-    }
-}
-
-impl std::fmt::Display for Key {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Key::W => write!(f, "Key::W"),
-            Key::A => write!(f, "Key::A"),
-            Key::S => write!(f, "Key::S"),
-            Key::D => write!(f, "Key::D"),
-            Key::R => write!(f, "Key::R"),
-            Key::N => write!(f, "Key::N"),
-            Key::F => write!(f, "Key::F"),
-            Key::I => write!(f, "Key::I"),
-            Key::LSHIFT => write!(f, "Key::LSHIFT"),
-            Key::RSHIFT => write!(f, "Key::RSHIFT"),
-            Key::SPACE => write!(f, "Key::SPACE"),
-            Key::ESC => write!(f, "Key::ESC"),
-            Key::NumPLUS => write!(f, "Key::NumPLUS"),
-            Key::NumMINUS => write!(f, "Key::NumMINUS"),
-            Key::MouseLeftButton => write!(f, "Key::MouseLeftButton"),
-            Key::MouseRightButton => write!(f, "Key::MouseRightButton"),
-            Key::MouseWheelUp => write!(f, "Key::MouseWheelUp"),
-            Key::MouseWheelDown => write!(f, "Key::MouseWheelDown"),
-            Key::NONE => write!(f, "Key::NONE"),
-        }
     }
 }
